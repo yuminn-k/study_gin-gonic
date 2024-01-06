cdc76aebc61f38c57c0df05814c0eb98d63ccc41
devYuMinKim <gimyumin40@gmail.com>
2024-01-07 03:58:46 +0900

    ✨ feat: Add short declaration operator examples

---
+++ src/short_declartion_operator/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x) // 42
+	x = 99
+	fmt.Println(x) // 99
+	y := 100 + 70
+	fmt.Println(y) // 170
+	z := "Bond, James"
+	fmt.Println(z) // "Bond, James"
+}
