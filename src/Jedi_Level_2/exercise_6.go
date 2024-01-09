13e137c0e70cc8b4e4de33dfea7fb7cadfa0f8b7
devYuMinKim <gimyumin40@gmail.com>
2024-01-09 21:56:36 +0900

    ✨ feat: Add constants for the last 4 years using iota

---
+++ src/Jedi_Level_2/exercise_6.go
@@ -0,0 +1,23 @@
+/*
+	Using iota, create 4 constants for the last 4 years. Print the constant values.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = iota + 2017
+	b = iota + 2017
+	c = iota + 2017
+	d = iota + 2017
+)
+
+func main() {
+	fmt.Println(a)	// 2017
+	fmt.Println(b)	// 2018
+	fmt.Println(c)	// 2019
+	fmt.Println(d)	// 2020
+}
