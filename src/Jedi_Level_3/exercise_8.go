f22e85aa1eaeff286cf4836432f92a041ddc609c
devYuMinKim <gimyumin40@gmail.com>
2024-01-09 22:29:59 +0900

    ✨ feat: Add switch statement with no switch expression

---
+++ src/Jedi_Level_3/exercise_8.go
@@ -0,0 +1,18 @@
+/*
+	Create a program that uses a switch statement with no switch expression specified.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch {
+	case false:
+		fmt.Println("This should not print")
+	case true:
+		fmt.Println("This should print")
+	}
+}
