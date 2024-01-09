5f48630c0eb32b9114bd2e0c39902dffd68d2aaf
devYuMinKim <gimyumin40@gmail.com>
2024-01-09 21:47:51 +0900

    ✨ feat: Add raw string literal example

---
+++ src/Jedi_Level_2/exercise_5.go
@@ -0,0 +1,14 @@
+/*
+	Create a variable of type string using a raw string literal. Print it.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := `This is a raw string literal`
+	fmt.Println(a)	// This is a raw string literal
+}
