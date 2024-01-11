5023b2b50b21db86e7297fb71ef4e505e494a30b
devYuMinKim <gimyumin40@gmail.com>
2024-01-11 16:53:53 +0900

    ✨ feat: Add delete operations to slice

--- src/slice/main.go
+++ src/slice/main.go
@@ -28,6 +28,7 @@ func main() {
 	fmt.Println(x[1:])	// [5 7 8 42]
 	fmt.Println(x[1:3])	// [5 7]
 
+	// APPEND
 	x = append(x, 77, 88, 99, 1014)	// append values to a slice
 
 	fmt.Println(x)	// [4 5 7 8 42 77 88 99 1014]
@@ -40,4 +41,9 @@ func main() {
 	x = append(x, y)
 
 	fmt.Println(x)	// [4 5 7 8 42 77 88 99 1014 234 456 678 987 [234 456 678 987]]
+
+	// DELETE
+	x = append(x[:2], x[4:]...)	// delete values from a slice
+
+	fmt.Println(x)	// [4 5 42 77 88 99 1014 234 456 678 987 [234 456 678 987]]
 }
