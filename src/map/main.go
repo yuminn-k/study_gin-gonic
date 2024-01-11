987fd1b28693a17c1acb69ac62479f192ccebd33
devYuMinKim <gimyumin40@gmail.com>
2024-01-11 18:44:48 +0900

    ✨ feat: Add map example

---
+++ src/map/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// map[key]value
+	m := map[string]int{
+		"James": 32,
+		"Miss Moneypenny": 27,
+	}
+
+	fmt.Println(m)	// map[James:32 Miss Moneypenny:27]
+	fmt.Println(m["James"])	// 32
+	fmt.Println(m["Barnabas"])	// 0
+
+	
+
+	v, ok := m["Barnabas"]
+	fmt.Println(v)	// 0
+	fmt.Println(ok)	// false
+
+	// if m["Barnabas"] exists
+	if v, ok := m["Barnabas"]; ok {
+		fmt.Println("THIS IS THE IF PRINT", v)
+	}
+
+	if v, ok := m["Miss Moneypenny"]; ok {
+		fmt.Println("THIS IS THE IF PRINT", v)	// THIS IS THE IF PRINT 27
+	}
+}
