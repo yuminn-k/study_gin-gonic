6d069ff9ef5c09857b0e2213a0e80d0a3f7bdbf5
devYuMinKim <gimyumin40@gmail.com>
2024-01-25 22:53:50 +0900

    :memo: docs: Add comments at item_service.go, item_controller.go

--- project_gin-gonic/gin-fleamarket/controllers/item_controller.go
+++ project_gin-gonic/gin-fleamarket/controllers/item_controller.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
+// IItemController IItemController 인터페이스는 ItemController 구조체가 구현해야 하는 메서드를 정의한다.
 type IItemController interface {
 	FindAll(ctx *gin.Context)
 }
 
+// ItemController ItemController 구조체는 IItemController 인터페이스를 구현한다.
 type ItemController struct {
 	service services.IItemService
 }
 
+// NewItemController NewItemController() 메서드는 ItemController 구조체를 생성한다.
 func NewItemController(service services.IItemService) IItemController {
 	return &ItemController{service: service}
 }
 
+// FindAll FindAll() 메서드는 모든 Item을 반환한다.
 func (c *ItemController) FindAll(ctx *gin.Context) {
 	items, err := c.service.FindAll()
 	if err != nil {
--- project_gin-gonic/gin-fleamarket/services/item_service.go
+++ project_gin-gonic/gin-fleamarket/services/item_service.go
@@ -5,18 +5,22 @@ import (
 	"gin-fleamarket/repositories"
 )
 
+// IItemService IItemService 인터페이스는 ItemService 구조체가 구현해야 하는 메서드를 정의한다.
 type IItemService interface {
 	FindAll() (*[]models.Item, error)
 }
 
+// ItemService ItemService 구조체는 IItemService 인터페이스를 구현한다.
 type ItemService struct {
 	repository repositories.IItemRepository
 }
 
+// NewItemService NewItemService() 메서드는 ItemService 구조체를 생성한다.
 func NewItemService(repository repositories.IItemRepository) IItemService {
 	return &ItemService{repository: repository}
 }
 
+// FindAll FindAll() 메서드는 모든 Item을 반환한다.
 func (s *ItemService) FindAll() (*[]models.Item, error) {
 	return s.repository.FindAll()
 }
