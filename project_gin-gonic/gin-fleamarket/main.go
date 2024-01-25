f4f6675a709057a3dca0b0afe130a9293a37db0d
devYuMinKim <gimyumin40@gmail.com>
2024-01-25 23:04:44 +0900

    :sparkles: feat: Implement FindAll logic

--- project_gin-gonic/gin-fleamarket/main.go
+++ project_gin-gonic/gin-fleamarket/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"gin-fleamarket/controllers"
 	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +15,11 @@ func main() {
 		{ID: 3, Name: "商品３", Price: 3000, Description: "説明３", SoldOut: false},
 	}
 
-	r := gin.Default() // gin.Default()는 기본적으로 Logger와 Recovery 미들웨어를 포함한 Gin 엔진을 생성한다. HTTP 요청을 받고 응답을 전송하는 역할을 한다.
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	}) // GET 요청을 받고, "/ping" 경로로 요청이 들어오면, JSON 형식으로 응답을 보내준다.
-	r.Run("localhost:8080") // 8080 포트로 서버를 실행한다.
+	itemRepository := repositories.NewItemMemoryRepository(items) // ItemMemoryRepository 구조체를 생성한다.
+	itemService := services.NewItemService(itemRepository)        // ItemService 구조체를 생성한다.
+	itemController := controllers.NewItemController(itemService)  // ItemController 구조체를 생성한다.
+
+	r := gin.Default()                      // gin.Default()는 기본적으로 Logger와 Recovery 미들웨어를 포함한 Gin 엔진을 생성한다. HTTP 요청을 받고 응답을 전송하는 역할을 한다.
+	r.GET("/items", itemController.FindAll) // GET 요청을 받고, "/items" 경로로 요청이 들어오면, itemController.FindAll() 메서드를 실행한다.
+	r.Run("localhost:8080")                 // 8080 포트로 서버를 실행한다.
 }
