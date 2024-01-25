b503cc03bafb7ffa50fee0412ada4a42e09c4154
devYuMinKim <gimyumin40@gmail.com>
2024-01-25 22:39:22 +0900

    :sparkles: feat: Implement Item model

--- project_gin-gonic/gin-fleamarket/main.go
+++ project_gin-gonic/gin-fleamarket/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品１", Price: 1000, Description: "説明１", SoldOut: false},
+		{ID: 2, Name: "商品２", Price: 2000, Description: "説明２", SoldOut: true},
+		{ID: 3, Name: "商品３", Price: 3000, Description: "説明３", SoldOut: false},
+	}
+
 	r := gin.Default() // gin.Default()는 기본적으로 Logger와 Recovery 미들웨어를 포함한 Gin 엔진을 생성한다. HTTP 요청을 받고 응답을 전송하는 역할을 한다.
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
